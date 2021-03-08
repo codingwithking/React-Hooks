dcd8010f7e5d5eb29919138db51c1abc7c24f59a
techtoking <techtoking@gmail.com>
2021-03-08 20:17:12 +0530

    master

--- react-state-hook/src/App.js
+++ react-state-hook/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
-import ClassCounter from './components/ClassCounter';
-import HooksCounter from './components/HooksCounter';
-import PrevStateCounter from './components/PrevStateCounter';
+// import ClassCounter from './components/ClassCounter';
+// import HooksCounter from './components/HooksCounter';
+// import PrevStateCounter from './components/PrevStateCounter';
 import UpdateObjectState from './components/UpdateObjectState';
 
 function App() {
